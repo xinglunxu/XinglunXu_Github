e0f515067657af003b16ee0e096686a601fabfe4
xinglunxu <xinglunxu@gmail.com>
2014-04-23 16:24:27 -0700

    ?

---
+++ JavaGUI/src/Controller.java
@@ -0,0 +1,18 @@
+public class Controller extends Jpanel
+{
+    private final JButton buttonOk = new JButton("OK");
+    private final JButton buttonCancel = new JButton("Cancel");
+    private final JButton buttonSetup = new JButton("Setup");
+    private final JButton buttonHelp = new JButton("Help");
+    private final JLabel labelInput = new JLabel("Printer:");
+    private final JLabel labelOutput = new JLabel("Console:");
+    private final JTextField textFieldInput = new JTextField();
+    private final JTextField textFieldOutput = new JTextField();
+
+    Controller()
+    {
+
+
+    }
+
+}
